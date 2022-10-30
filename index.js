192f24ee83038c4d4001d83313ae5e95b1ace2f5
theMstafa <mm22180481@gmail.com>
2022-10-30 05:38:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(droiversArr, name) {
+    return droiversArr.filter(
+       (matching) => matching.toLowerCase() === name.toLowerCase()
+    );
+  }
+  function fuzzyMatch(droiversArr ,testString){
+    return droiversArr.filter(
+        (possibleMatch) =>
+          possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+      );
+  }
+  function matchName(droiversArr, name) {
+    return droiversArr.filter((matchName) => matchName.name === name);
+  }
